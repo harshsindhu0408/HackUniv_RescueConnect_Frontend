27ba8b1c5fd5840438efa48e459f959622b31781
Harsh <61960651+harshsindhu0408@users.noreply.github.com>
2023-11-30 11:45:16 +0530

    changes in api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8080/api/n1";
+const BASE_URL = "https://hackuniv-rescueconnect-backend.onrender.com/api/n1";
 
 // AGENCY END POINTS
 export const agencyEndPoints = {
